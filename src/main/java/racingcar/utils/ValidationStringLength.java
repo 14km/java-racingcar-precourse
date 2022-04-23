05d31588c2eeea5e5b118f14f34e96842c764d3f
14km <k.connor614@gmail.com>
2022-04-24 01:49:08 +0900

    refactor: 사용하지 않는 util 제거

--- src/main/java/racingcar/utils/ValidationStringLength.java
+++
@@ -1,9 +0,0 @@
-package racingcar.utils;
-
-public class ValidationStringLength {
-    private static final int MAX_LENGTH = 5;
-
-    public static boolean validationLength(String input) {
-       return input.length() <= MAX_LENGTH;
-    }
-}
