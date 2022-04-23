da195840f688dd4d07723727e23839b62f7bc741
14km <k.connor614@gmail.com>
2022-04-23 22:44:07 +0900

    refactor: RacingCar 등록 방식 변경 및 중복된 테스트 제거

--- src/main/java/racingcar/domain/RacingCars.java
+++ src/main/java/racingcar/domain/RacingCars.java
@@ -6,11 +6,8 @@ import java.util.List;
 public class RacingCars {
     private final List<RacingCar> racingCars = new ArrayList<>();
 
-    public void addRacingCar(String[] names) {
-        for (String name : names) {
-            RacingCar racingCar = new RacingCar(new RacingCarName(name));
-            racingCars.add(racingCar);
-        }
+    public void addRacingCar(RacingCar racingCar) {
+        racingCars.add(racingCar);
     }
 
     public List<RacingCar> getRacingCars() {
--- src/test/java/racingcar/domain/RacingCarsTest.java
+++ src/test/java/racingcar/domain/RacingCarsTest.java
@@ -2,12 +2,8 @@ package racingcar.domain;
 
 import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
-import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.ValueSource;
-import racingcar.common.Messages;
-
-import static org.assertj.core.api.AssertionsForClassTypes.assertThatExceptionOfType;
 
 class RacingCarsTest {
     private RacingCars racingCars;
@@ -20,43 +16,14 @@ class RacingCarsTest {
     @ParameterizedTest
     @ValueSource(strings = {"abc,null,test"})
     void 레이싱_자동차_설정(String input) {
-        String[] strings = input.split(",");
+        String[] names = input.split(",");
 
-        racingCars.addRacingCar(strings);
+        for (String name : names) {
+            RacingCar racingCar = new RacingCar(new RacingCarName(name));
+            racingCars.addRacingCar(racingCar);
+        }
 
         // then
         Assertions.assertThat(racingCars.getRacingCars().size()).isEqualTo(3);
-    }
-
-    @ParameterizedTest
-    @ValueSource(strings = {"abcdefg,car"})
-    void 자동차_등록_중_이름_글자수를_초과한_경우(String input) {
-        String[] strings = input.split(",");
-
-        // then
-        assertThatExceptionOfType(IllegalArgumentException.class)
-                .isThrownBy(() -> racingCars.addRacingCar(strings))
-                .withMessageContaining(Messages.EXCEED_CAR_NAME_MAX_LENGTH);
-    }
-
-    @ParameterizedTest
-    @ValueSource(strings = {"abc,def,,car"})
-    void 자동차_등록_중_이름_비어있는_경우(String input) {
-        String[] strings = input.split(",");
-
-        // then
-        assertThatExceptionOfType(IllegalArgumentException.class)
-                .isThrownBy(() -> racingCars.addRacingCar(strings))
-                .withMessageContaining(Messages.CAR_NAME_NOT_EMPTY);
-    }
-
-    @Test
-    void 자동차_등록_중_이름_NULL_경우() {
-        String[] strings = {"abb", "deff", null, "gg"};
-
-        // then
-        assertThatExceptionOfType(NullPointerException.class)
-                .isThrownBy(() -> racingCars.addRacingCar(strings))
-                .withMessageContaining(Messages.CAR_NAME_NOT_NULL);
     }
 }
