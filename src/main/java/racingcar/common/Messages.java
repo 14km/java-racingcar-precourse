29ad756ccbd9db579af9b3e63977d6b128a7061c
14km <k.connor614@gmail.com>
2022-04-24 00:37:31 +0900

    feat: RacingResult 추가

--- src/main/java/racingcar/common/Messages.java
+++ src/main/java/racingcar/common/Messages.java
@@ -3,6 +3,7 @@ package racingcar.common;
 public class Messages {
     public static final String START_MESSAGE = "경주할 자동차 이름을 입력하세요. (이름은 쉼표(,)기준으로 구분)";
     public static final String REPEAT_COUNT_SETTING = "경주 시도할 횟수";
+    public static final String RACING_WINNER = "최종 우승자:";
 
     public static final String EXCEED_CAR_NAME_MAX_LENGTH = "[ERROR] 자동차 이름은 5자 이하만 가능합니다.";
     public static final String CAR_NAME_NOT_NULL = "[ERROR] 자동차 이름은 NULL로 설정할 수 없습니다.";
