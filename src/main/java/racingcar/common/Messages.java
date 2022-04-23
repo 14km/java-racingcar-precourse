f882f78f2ed7800b039d5a7556944bf48d3580bd
14km <k.connor614@gmail.com>
2022-04-23 13:22:46 +0900

    feat:Messages 에러유형 추가정의

--- src/main/java/racingcar/common/Messages.java
+++ src/main/java/racingcar/common/Messages.java
@@ -3,4 +3,6 @@ package racingcar.common;
 public class Messages {
 
     public static final String EXCEED_CAR_NAME_MAX_LENGTH = "[ERROR] 자동차 이름은 5자 이하만 가능합니다.";
+    public static final String CAR_NAME_NOT_NULL = "[ERROR] 자동차 이름은 NULL로 설정할 수 없습니다.";
+    public static final String CAR_NAME_NOT_EMPTY = "[ERROR] 자동차 이름은 필수로 지정해야합니다.";
 }
