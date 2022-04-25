220036bf87fa3330f29fa33b514d8144e502e61e
14km <k.connor614@gmail.com>
2022-04-25 17:26:17 +0900

    test: 우승자가 2명일때 테스트 추가

--- src/test/java/racingcar/domain/RacingResultTest.java
+++ src/test/java/racingcar/domain/RacingResultTest.java
@@ -21,7 +21,7 @@ class RacingResultTest {
     }
 
     @Test
-    void 결과를_출력한다() {
+    void 우승자가_한명일_경우_결과를_출력한다() {
         // given
         List<RacingCar> racingCars = this.racingCars.getRacingCars();
         racingCars.get(0).move(9);
@@ -35,5 +35,26 @@ class RacingResultTest {
 
         // then
         Assertions.assertThat(winners).contains("최종 우승자: A");
+    }
+
+    @Test
+    void 우승자가_두명일_경우_결과를_출력한다() {
+        // given
+        List<RacingCar> racingCars = this.racingCars.getRacingCars();
+        racingCars.get(0).move(9);
+        racingCars.get(0).move(9);
+        racingCars.get(0).move(9);
+
+        racingCars.get(1).move(9);
+        racingCars.get(1).move(9);
+        racingCars.get(1).move(9);
+
+        RacingResult racingResult = new RacingResult(racingCars);
+
+        // when
+        String winners = racingResult.winners();
+
+        // then
+        Assertions.assertThat(winners).contains("최종 우승자: A,B");
     }
 }
